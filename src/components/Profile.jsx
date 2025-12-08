4d0f88be092fe55c40b17a81e4f0e7b887b4adc6
mayaarafa <marafa@my.bcit.ca>
2025-12-08 06:24:01 -0800

    undo conditional

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,7 +5,15 @@ import { useUser } from "@auth0/nextjs-auth0/client";
 export default function Profile() {
   const { user, isLoading } = useUser();
 
-  if (!isLoading && !user) {
+  if (isLoading) {
+    return (
+      <div className="loading-state">
+        <div className="loading-text">Loading user profile...</div>
+      </div>
+    );
+  }
+
+  if (!user) {
     return null;
   }
 
